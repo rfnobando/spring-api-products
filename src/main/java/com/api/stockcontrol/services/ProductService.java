4793548f3c1a347338b340c6a77b5692228d5a5c
Rodrigo <rfnobando@gmail.com>
2024-03-26 02:18:53 -0300

    show method added to ProductService

--- src/main/java/com/api/stockcontrol/services/ProductService.java
+++ src/main/java/com/api/stockcontrol/services/ProductService.java
@@ -8,6 +8,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.Optional;
 
 @Service
 public class ProductService {
@@ -21,6 +22,10 @@ public class ProductService {
 
     public Product store(Product product) {
         return productRepository.save(product);
+    }
+
+    public Optional<Product> show(Long id) {
+        return productRepository.findById(id);
     }
 
 }
