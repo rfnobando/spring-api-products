61203c9a57a37a5c5aba8398c863e2380b088189
Rodrigo <rfnobando@gmail.com>
2024-03-26 02:11:57 -0300

    index method has been created with pagination

--- src/main/java/com/api/stockcontrol/controllers/ProductController.java
+++ src/main/java/com/api/stockcontrol/controllers/ProductController.java
@@ -3,10 +3,11 @@ package com.api.stockcontrol.controllers;
 import com.api.stockcontrol.models.Product;
 import com.api.stockcontrol.services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
 
 @RestController
 @RequestMapping("api/products")
@@ -14,6 +15,11 @@ public class ProductController {
 
     @Autowired
     ProductService productService;
+
+    @GetMapping
+    public Page<Product> index(Pageable pageable) {
+        return this.productService.index(pageable);
+    }
 
     @PostMapping
     public Product store(@RequestBody Product product) {
--- src/main/java/com/api/stockcontrol/services/ProductService.java
+++ src/main/java/com/api/stockcontrol/services/ProductService.java
@@ -3,13 +3,21 @@ package com.api.stockcontrol.services;
 import com.api.stockcontrol.models.Product;
 import com.api.stockcontrol.repositories.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
 
 @Service
 public class ProductService {
 
     @Autowired
     ProductRepository productRepository;
+
+    public Page<Product> index(Pageable pageable) {
+        return productRepository.findAll(pageable);
+    }
 
     public Product store(Product product) {
         return productRepository.save(product);
