fdbb707f15b36b189926e2ea2386ff063412083f
Rodrigo <rfnobando@gmail.com>
2024-03-26 00:56:59 -0300

    store method was created successfully

--- src/main/java/com/api/stockcontrol/services/ProductService.java
+++ src/main/java/com/api/stockcontrol/services/ProductService.java
@@ -1,5 +1,6 @@
 package com.api.stockcontrol.services;
 
+import com.api.stockcontrol.models.Product;
 import com.api.stockcontrol.repositories.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -10,5 +11,8 @@ public class ProductService {
     @Autowired
     ProductRepository productRepository;
 
+    public Product store(Product product) {
+        return productRepository.save(product);
+    }
 
 }
