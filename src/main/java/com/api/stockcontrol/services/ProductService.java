d0c37b3ac26d4edd62fe28c15c28aace6b88100a
Rodrigo <rfnobando@gmail.com>
2024-03-25 23:55:30 -0300

    ProductService has been created

---
+++ src/main/java/com/api/stockcontrol/services/ProductService.java
@@ -0,0 +1,14 @@
+package com.api.stockcontrol.services;
+
+import com.api.stockcontrol.repositories.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProductService {
+
+    @Autowired
+    ProductRepository productRepository;
+
+
+}
